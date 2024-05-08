d71650ffd5f973a551a9625768bbea23ed512179
Ayush Patel <94599756+Ayush472@users.noreply.github.com>
2024-05-08 17:30:50 +0000

    corulse create and logo update

--- components/landingprofile/page.jsx
+++ components/landingprofile/page.jsx
@@ -1,47 +1,78 @@
+"use client";
+import React, { useState } from "react";
+import Slider from "react-slick";
+// import img from "../../assets/Images/industry.jpeg";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./LandingProfile.css"; // Import your CSS file for custom styling
+import slide1 from "../../assets/Images/slide1.jpeg";
+import slide2 from "../../assets/Images/slide2.jpeg";
+import slide3 from "../../assets/Images/slide3.jpeg";
 import Image from "next/image";
-import img from "../../assets/Images/industry.jpeg";
+import 'animate.css';
 
 const items = [
   {
-    title: "Slide 1",
-    description: "Description for slide 1",
-    image: img,
+    title: "Effective Electrical",
+    description: " Solutions ",
+    image: slide1,
+    color: "#06826f",
   },
   {
-    title: "Slide 2",
-    description: "Description for slide 2",
-    image: img,
+    title: "Industrial Electrical &",
+    description: "  Electronics Goods.  ",
+    image: slide2,
+    color: "#e84949",
   },
-  // Add more items as needed
+  {
+    title: "Empowering",
+    description: "You Safely ",
+    image: slide3,
+    color: "#b1bf35",
+  },
 ];
+
 const LandingProfile = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
   const settings = {
     dots: true,
     infinite: true,
-    speed: 500,
+    speed: 1000,
     slidesToShow: 1,
     slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3000,
+    // autoplay: true,
+    // autoplaySpeed: 3000,
   };
 
   return (
-    <></>
-    // <div className="bg-bgMain">
-    //   <div className="flex flex-wrap p-5 py-12">
-    //     <div className="w-1/2 p-3">
-    //       <div className="text-blueMain font-extrabold text-6xl tracking-wider">
-    //         HIGH QUALITY PRODUCTS
-    //       </div>
-    //       <div className="bg-redMain font-extrabold text-4xl tracking-wider mt-4">
-    //         High Quality Products for High Perfomance
-    //       </div>
-    //     </div>
-    //     <div className="w-1/2">
-    //       <Image src={img} alt="" width={500} height={500} />
-    //     </div>
-    //   </div>
-    // </div>
+    <div className="carousel-container">
+      <Slider {...settings}>
+        {items.map((item, index) => (
+          <div key={index} className="relative">
+            <div className="text-5xl		font-bold	">
+              <div
+                className="absolute left-1/4 top-1/3 transition-opacity duration-500 ease-in-out animate__slideInLeft"
+                style={{ color: item.color }}
+              >
+                <h1 className="animate__slideInLeft">{item.title}</h1>
+                <h1 className="animate__slideInLeft">{item.description}</h1>
+              </div>
+              <div className="image-wrapper" style={{ height: "500px" }}>
+                <Image
+                  src={item.image}
+                  alt={item.title}
+                  height="auto"
+                  objectFit="cover"
+                  style={{ width: "100%" }}
+                />
+              </div>
+            </div>
+          </div>
+        ))}
+      </Slider>
+    </div>
   );
 };
+
 export default LandingProfile;
